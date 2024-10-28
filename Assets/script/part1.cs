c6f38f0d50239a31058a9abb573d98fc6df6e670
JanaBarakeh <sbarakeh15@gmail.com>
2024-10-28 22:44:23 +0200

    try commit

---
+++ Assets/script/part1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class part1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
